919d106af1dd7b9459d8e47414e41632ee5732ad
sandro <sandro.grizzo@desy.de>
2024-05-22 11:59:50 +0200

    prepare for switch to gitlab

--- src/main/java/phases/monitor/MonitorSandro.java
+++ src/main/java/phases/monitor/MonitorSandro.java
@@ -9,7 +9,7 @@ public class MonitorSandro {
 
     private final static String OS_TYPE = System.getProperty("os.name").toLowerCase();
     public MonitorSandro() throws IOException {
-        System.out.printf("OS_TYPE is: %24s%n!", OS_TYPE);
+        System.out.printf("OS_TYPE is: %24s%n", OS_TYPE);
 
         if (!OS_TYPE.contains("nux") && !OS_TYPE.contains("darwin")) {
             System.out.println("OS not supported!");
@@ -26,7 +26,7 @@ public class MonitorSandro {
 
     /**
      * This method returns a json object containing the status of the system's disk
-     * @return JSONObject or null
+     * @return JSONObject (can be empty, if keys or values are null) or null
      * @throws IOException
      * @throws NullPointerException
      * @throws UnsupportedOperationException
@@ -35,6 +35,7 @@ public class MonitorSandro {
     public JSONObject getDiskInfo() throws IOException, NullPointerException, UnsupportedOperationException, JSONException {
 
         JSONObject jsonObject = null;
+        JSONObject analyseJSON = new JSONObject();
 
         if (OS_TYPE.contains("nux")) {
 
@@ -44,19 +45,27 @@ public class MonitorSandro {
             ProcessBuilder pb2 = new ProcessBuilder("sudo", "smartctl", "-j", "-a", "/dev/" + device);
             Process proc2 = pb2.start();
             String jsonString = new String(proc2.getInputStream().readAllBytes());
-//            System.out.println(jsonString);
+            if (jsonString == null || jsonString.isEmpty()) {
+                System.out.println("No disk information found!");
+                return null;
+            }
             jsonObject = new JSONObject(jsonString);
-//            System.out.println(jsonObject);
-            String model_name = jsonObject.get("model_name").toString();
-            Object num_err_log_entries = jsonObject.getJSONObject("nvme_smart_health_information_log").get("num_err_log_entries");
-            Object media_errors = jsonObject.getJSONObject("nvme_smart_health_information_log").get("media_errors");
-            Object critical_warning = jsonObject.getJSONObject("nvme_smart_health_information_log").get("critical_warning");
+            Object model_name = jsonObject.opt("model_name");
+            Object num_err_log_entries = jsonObject.optJSONObject("nvme_smart_health_information_log", new JSONObject()).opt("num_err_log_entries");
+            Object media_errors = jsonObject.optJSONObject("nvme_smart_health_information_log", new JSONObject()).opt("media_errors");
+            Object critical_warning = jsonObject.optJSONObject("nvme_smart_health_information_log", new JSONObject()).opt("critical_warning");
+            System.out.println();
             System.out.printf("Disk device name: %20s%n", device);
             System.out.printf("Model name: %49s%n", model_name);
             System.out.printf("Number of Error-log entries: %5d%n", num_err_log_entries);
             System.out.printf("Media Errors: %18d%n", media_errors);
             System.out.printf("Critical Warning: %14d%n", critical_warning);
-//            System.out.println(jsonObject.getJSONObject("temperature").get("current"));
+
+            analyseJSON.putOpt("model_name", model_name);
+            analyseJSON.putOpt("num_err_log_entries", num_err_log_entries);
+            analyseJSON.putOpt("media_errors", media_errors);
+            analyseJSON.putOpt("critical_warning", critical_warning);
+            System.out.println(analyseJSON);
         }
         else if (OS_TYPE.contains("darwin")) {
             //todo: implement for MacOS
@@ -73,7 +82,7 @@ public class MonitorSandro {
 //            System.out.println(jsonObject.get("serial_number"));
 //            System.out.println(jsonObject.getJSONObject("temperature").get("current"));
         }
-        return jsonObject;
+        return analyseJSON;
     }
     public JSONObject parseSmartctlOutput(JSONObject jsonObject) {
         return new JSONObject(jsonObject);
